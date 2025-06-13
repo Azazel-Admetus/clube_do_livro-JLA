e7a46df7b8ebe34caae2a839cf79166d93c5d375
Gustavo de Souza Vieira <gustavo@CamOPO2.com>
2025-06-13 08:33:51 -0400

    terminei a página de cadastro. Vou fazer a login e aí vou passando fazendo higienização dos campos e colocando tratamento de erro na página html

--- php/cadastro.php
+++ php/cadastro.php
@@ -7,9 +7,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 
     if(!empty($nome) && !empty($email) && !empty($senha)){
-        $stmt = $conn->prepare("INSERT INTO users (nome, email, senha) VALUES (:nome, :email, :senha)");
-        $stmt->bindValue(':nome', $nome);
-        $stmt->bindValue(':email', $email);
-        $stmt->bindValue(':senha', $senha);
+        $stmt = $conn->prepare('SELECT COUNT(*) FROM users WHERE nome = :nome OR email = :email');
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $exists = $stmt->fetchColumn();
+        if ($exists > 0){
+            header("Location:../html/cadastro.html?error=credencias_existentes");
+            exit();
+        } else {
+            $stmt = $conn->prepare("INSERT INTO users (nome, email, senha) VALUES (:nome, :email, :senha)");
+            $stmt->bindParam(':nome', $nome);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':senha', $senha);
+            if ($stmt->execute()){
+                session_start();
+                $_SESSION['username'] = $nome;
+                $_SESSION['email'] = $email;
+                header("Location:../html/home.html?processo=sucesso");
+                exit();
+            } else{
+                header('Location:../html/cadastro.html?error=erro_cadastro');
+                exit();
+            }
+        }
+    } else{
+        header('Location:../html/cadastro.html?error=campos_vazios');
+        exit();
     }
 }
